c8a8db73fc5a9708c326450da4467b3c6e03fb36
kayali86 <a.kayali@mail.de>
2018-02-14 20:46:49 +0100

    Ready to Submit

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -24,45 +24,33 @@ public class MainActivity extends AppCompatActivity {
         callView =  (TextView) findViewById(R.id.call_view);
         websiteView = (TextView) findViewById(R.id.website_view);
         youtubeView = (ImageView) findViewById(R.id.youtube_view);
+    }
 
 
-        // Opens the dialer to call Udacity
-        callView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String phoneNumber = "650 555 5555";
+    // Opens the dialer to call Udacity
+    public void call(View view) {
+        String phoneNumber = "650 555 5555";
 
-                    Intent intent = new Intent(Intent.ACTION_DIAL);
-                    intent.setData(Uri.parse("tel:" + phoneNumber));
-                    if (intent.resolveActivity(getPackageManager()) != null) {
-                        startActivity(intent);
-                    }
-            }
-        });
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
+    // Go to Udacity's Website
+    public void goToWebsite(View view) {
+        String url = "http://www.udacity.com";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
 
-
-        // Go to Udacity's Website
-        websiteView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = "http://www.udacity.com";
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
-            }
-        });
-
-
-        // Go to Udacity's Youtube Channel
-        youtubeView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = "https://www.youtube.com/user/Udacity";
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
-            }
-        });
+    // Go to Udacity's Youtube Channel
+    public void goToYoutube(View view) {
+        String url = "https://www.youtube.com/user/Udacity";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
